e5ed42f88470e2eeec995a788ba1f128fd06187d
akinmix <akinmidi@gmail.com>
2025-04-25 13:53:44 +0300

    Update ilan_detay.py

--- routes/ilan_detay.py
+++ routes/ilan_detay.py
@@ -1,4 +1,4 @@
-from controllers.ilan_controller import prepare_ilan_dosyasi_firecrawl  # eğer kullanılmayacaksa silebilirsin
+from controllers.ilan_controller import prepare_ilan_dosyasi_firecrawl
 import os
 import requests
 from fastapi import APIRouter, HTTPException
